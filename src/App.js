cf8447e6ef78ac72a72a5ab0ec2999bc43da8b56
heyjaisal <zolvebanking@gmail.com>
2024-07-27 13:53:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './components/Main.js';
+import './components/style.css';
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
